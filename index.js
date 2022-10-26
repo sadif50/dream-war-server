4fa260786c1062c566ca1d8f316f9ae098d1c3d6
sadif50 <smsujon50@gmail.com>
2022-10-26 12:36:45 +0600

    Send courses data for courses route

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const courses = require('./src/data.json');
+
 app.get('/', (req, res) => {
     res.send("Welcome ! Server Running ! !");
 });
 
-
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log('server runnig on', port);
