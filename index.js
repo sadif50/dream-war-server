333ecbf0c5e4bbd0b42f6bf28b032ba141c9fa60
sadif50 <smsujon50@gmail.com>
2022-10-26 13:05:36 +0600

    Use cors for share data with everyone and add comments on code

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
+// Using cors for share data with everyone
+app.use(cors());
+
+// Require JSON fake data.
 const courses = require('./src/data.json');
 
 app.get('/', (req, res) => {
     res.send("Welcome ! Server Running ! !");
 });
 
+// Send All course data for /course route
 app.get('/courses', (req, res) => {
     res.send(courses);
 });
 
+// Send single course data for /course/:id route dynamically
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(course => course._id === id);
     res.send(selectedCourse);
 })
 
+// Send single course data for /checkout/:id route dynamically
 app.get('/checkout/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(course => course._id === id);
     res.send(selectedCourse);
 })
 
+// Listen app on the port
 app.listen(port, () => {
     console.log('server runnig on', port);
 })
