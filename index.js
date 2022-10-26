67cddbfcd19ea61eab3f084d9fc123d6f6619842
sadif50 <smsujon50@gmail.com>
2022-10-26 12:49:36 +0600

    single course data send by dynamic id

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
